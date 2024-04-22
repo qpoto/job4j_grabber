2eb7c96d002e19734a56961025255450d4a29ca7
Стас <qpoto@yandex.ru>
2024-04-22 21:46:52 +0300

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1  [#285210 #492646] Рефакторинг, изменение на 5 страниц

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -16,7 +16,7 @@ public class HabrCareerParse {
     public static final int PAGES = 5;
 
     public static void main(String[] args) throws IOException {
-        for (int pageNumber = 1; pageNumber < PAGES; pageNumber++) {
+        for (int pageNumber = 1; pageNumber <= PAGES; pageNumber++) {
             String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
             Connection connection = Jsoup.connect(fullLink);
             Document document = connection.get();
