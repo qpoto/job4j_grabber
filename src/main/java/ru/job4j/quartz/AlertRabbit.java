a8483dd4439d95711cf4667f2608e25ddf11efdb
Kondratev-SV <kondratevsv@intech.rshb.ru>
2024-04-22 08:40:19 +0300

    1.1. Job c параметрами [#260360 #492409]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -5,6 +5,7 @@ import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.*;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.newJob;
@@ -13,10 +14,11 @@ import static org.quartz.TriggerBuilder.newTrigger;
 
 public class AlertRabbit {
     public static void main(String[] args) {
-        try {
+        try (Connection connection = getConnection()) {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDetail job = newJob(Rabbit.class).build();
+            job.getJobDataMap().put("connection", connection);
             SimpleScheduleBuilder times = simpleSchedule()
                     .withIntervalInSeconds(setIntervalFromPropFile())
                     .repeatForever();
@@ -25,7 +27,9 @@ public class AlertRabbit {
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException | IOException se) {
+            Thread.sleep(10000);
+            scheduler.shutdown();
+        } catch (Exception se) {
             se.printStackTrace();
         }
     }
@@ -33,7 +37,16 @@ public class AlertRabbit {
     public static class Rabbit implements Job {
         @Override
         public void execute(JobExecutionContext context) {
-            System.out.println("Rabbit runs here ...");
+            try {
+                Connection connection = (Connection) context.getJobDetail().getJobDataMap().get("connection");
+                long millis = System.currentTimeMillis();
+                Timestamp timestamp = new Timestamp(millis);
+                PreparedStatement pS = connection.prepareStatement("INSERT INTO rabbit (created_date) VALUES (?)");
+                pS.setTimestamp(1, timestamp);
+                pS.execute();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
@@ -41,5 +54,15 @@ public class AlertRabbit {
         Properties properties = new Properties();
         properties.load(new FileReader("src/main/resources/rabbit.properties"));
         return Integer.parseInt(properties.getProperty("rabbit.interval"));
+    }
+
+    private static Connection getConnection() throws Exception {
+        Properties properties = new Properties();
+        properties.load(new FileReader("src/main/resources/rabbit.properties"));
+        Class.forName(properties.getProperty("driver_class"));
+        String url = properties.getProperty("url");
+        String login = properties.getProperty("login");
+        String password = properties.getProperty("password");
+        return DriverManager.getConnection(url, login, password);
     }
 }
