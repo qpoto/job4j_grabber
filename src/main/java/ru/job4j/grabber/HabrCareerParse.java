588d49febd6b456459948b394215cff76aa0c82b
Стас <qpoto@yandex.ru>
2024-04-22 21:35:11 +0300

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1  [#285210]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -13,19 +13,22 @@ public class HabrCareerParse {
     private static final String SOURCE_LINK = "https://career.habr.com";
     public static final String PREFIX = "/vacancies?page=";
     public static final String SUFFIX = "&q=Java%20developer&type=all";
+    public static final int PAGES = 5;
 
     public static void main(String[] args) throws IOException {
-        int pageNumber = 1;
-        String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
-        Connection connection = Jsoup.connect(fullLink);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            String date = row.select(".basic-date").attr("datetime");
-            String vacancyName = row.select(".vacancy-card__title").first().text();
-            Element linkElement = row.select(".vacancy-card__inner").first();
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            System.out.printf("%s %s %s%n", date, vacancyName, link);
-        });
+        for (int pageNumber = 1; pageNumber < PAGES; pageNumber++) {
+            String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
+            Connection connection = Jsoup.connect(fullLink);
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                String date = row.select(".basic-date").attr("datetime");
+                String vacancyName = row.select(".vacancy-card__title").first().text();
+                Element linkElement = row.select(".vacancy-card__inner").first();
+                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                System.out.printf("%s %s %s%n", date, vacancyName, link);
+            });
+        }
+
     }
 }
