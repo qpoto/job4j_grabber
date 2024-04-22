d14e6540212734a844b39faa7059afe494f7ad63
Стас <qpoto@yandex.ru>
2024-04-22 22:05:46 +0300

    2.1. Преобразование даты [#289476 #492645]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -1,12 +1,18 @@
 package ru.job4j.grabber.utils;
 
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        String[] dateTimeNoTimeZone = parse.split("\\+");
-        return LocalDateTime.parse(dateTimeNoTimeZone[0]);
+        OffsetDateTime offsetDateTime = OffsetDateTime.parse(parse);
+        return offsetDateTime.toLocalDateTime();
+    }
+
+    public static void main(String[] args) {
+        HabrCareerDateTimeParser habrCareerDateTimeParser = new HabrCareerDateTimeParser();
+        System.out.println(habrCareerDateTimeParser.parse("2024-04-20T12:30:03+03:00"));
     }
 }
