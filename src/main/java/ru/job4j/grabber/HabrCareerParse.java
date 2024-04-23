e89f0a6c6ee0e307d8bb4065b91b20238f3d09b7
Стас <qpoto@yandex.ru>
2024-04-23 21:48:17 +0300

    2.4. HabrCareerParse [#285213]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,39 +5,61 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
     public static final String PREFIX = "/vacancies?page=";
     public static final String SUFFIX = "&q=Java%20developer&type=all";
-    public static final int PAGES = 1;
+    public static final int PAGES = 5;
+    private final DateTimeParser dateTimeParser;
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
 
     public static void main(String[] args) throws IOException {
+        DateTimeParser dateTimeParser = new HabrCareerDateTimeParser();
+        Parse parse = new HabrCareerParse(dateTimeParser);
+        List<Post> posts = parse.list(
+                "НЕ ПОЙМУ, ЧТО ВЫ ХОТИТЕ ЧТОБЫ Я СЮДА ПЕРЕДОВАЛ? КАКОЙ ЛИНК?"
+                        + "ПО ИДЕЕ ЭТО ДОЛЖНА БЫТЬ ССЫЛКА НА СТРАНИЦУ С ВАКАНСИЯМИ, НО"
+                        + "ТОГДА МЕТОД ВЕРНЕТ ВАКАНСИИ ТОЛЬКО С ОДНОЙ СТРАНИЦЫ :-)");
+    }
+
+    @Override
+    public List<Post> list(String link) throws IOException {
+        List<Post> posts = new ArrayList<>();
         for (int pageNumber = 1; pageNumber <= PAGES; pageNumber++) {
             String fullLinkToVacancies = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
             Connection connection = Jsoup.connect(fullLinkToVacancies);
             Document document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
+
             rows.forEach(row -> {
-                String date = row.select(".basic-date").attr("datetime");
-                String name = row.select(".vacancy-card__title").first().text();
+                int id = 1;
+                String title = row.select(".vacancy-card__title").first().text();
                 String linkVacancy = row.select(".vacancy-card__title-link").attr("href");
                 String fullLink = String.format("%s%s", SOURCE_LINK, linkVacancy);
+                LocalDateTime date = dateTimeParser.parse(row.select(".basic-date").attr("datetime"));
                 try {
-                    System.out.printf("%s %s %s%n", date, name, fullLink);
-                    System.out.println("Описание");
-                    System.out.println(retrieveDescription(fullLink));
+                    String description = retrieveDescription(fullLink);
+                    posts.add(new Post(id++, title, fullLink, description, date));
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
             });
         }
+        return posts;
     }
+
     private static String retrieveDescription(String fullLink) throws IOException {
         List<String> tags = List.of("p", "h3", "li", "p");
         StringBuilder vacancyDescription = new StringBuilder();
@@ -56,4 +78,6 @@ public class HabrCareerParse {
         }
         return vacancyDescription.toString();
     }
+
+
 }
