293b78e54a0a5b3ae4859ab4a32b7368d894ed28
Стас <qpoto@yandex.ru>
2024-04-25 19:36:44 +0300

    5. PsqlStore [#285209]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -10,6 +10,9 @@ public class Post {
     private String description;
     private LocalDateTime created;
 
+    public Post() {
+    }
+
     public Post(int id, String title, String link, String description, LocalDateTime created) {
         this.id = id;
         this.title = title;
---
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -0,0 +1,91 @@
+package ru.job4j.grabber;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store {
+
+    private Connection connection;
+
+    public PsqlStore(Properties config) throws SQLException {
+        try {
+            Class.forName(config.getProperty("jdbc.driver"));
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        connection = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/postgres",
+                "postgres",
+                "password"
+        );
+    }
+
+    @Override
+    public void save(Post post) {
+        try (PreparedStatement pS =
+                     connection.prepareStatement(
+                             "INSERT INTO post(name, text, link, created) VALUES (?, ?, ?, ?)",
+                             Statement.RETURN_GENERATED_KEYS)) {
+            pS.setString(1, post.getTitle());
+            pS.setString(2, post.getDescription());
+            pS.setString(3, post.getLink());
+            pS.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
+            pS.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        List<Post> posts = new ArrayList<>();
+        try (PreparedStatement pS =
+                     connection.prepareStatement("Select * from post")) {
+            pS.execute();
+            ResultSet rS = pS.getResultSet();
+            while (rS.next()) {
+                Post postFromDB = getPost(rS);
+                posts.add(postFromDB);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return posts;
+    }
+
+    private static Post getPost(ResultSet rS) throws SQLException {
+        int id = rS.getInt("id");
+        String name = rS.getString("name");
+        String text = rS.getString("text");
+        String link = rS.getString("link");
+        Timestamp timestamp = rS.getTimestamp("created");
+        LocalDateTime dateTime = timestamp.toLocalDateTime();
+        return new Post(id, name, link, text, dateTime);
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post post = new Post();
+        try (PreparedStatement pS =
+                     connection.prepareStatement("Select * from post where id = (?)", id)) {
+            pS.execute();
+            ResultSet rS = pS.getResultSet();
+            while (rS.next()) {
+                post = getPost(rS);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+}
