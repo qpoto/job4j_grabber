6cf3bebed02c74843ce9fade4bb78e1dac474f69
Стас <qpoto@yandex.ru>
2024-04-26 21:38:01 +0300

    7. Web. [#289478 #493143]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -6,6 +6,9 @@ import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.List;
 import java.util.Properties;
 
@@ -14,20 +17,13 @@ import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 import static org.quartz.TriggerBuilder.newTrigger;
 
 public class Grabber implements Grab {
-    private final Parse parse;
-    private final Store store;
-    private final Scheduler scheduler;
-    private final int time;
+    private Properties cfg;
 
-    public Grabber(Parse parse, Store store, Scheduler scheduler, int time) {
-        this.parse = parse;
-        this.store = store;
-        this.scheduler = scheduler;
-        this.time = time;
+    public Grabber() {
     }
 
     @Override
-    public void init() throws SchedulerException {
+    public void init(Parse parse, Store store, Scheduler scheduler) throws SchedulerException {
         JobDataMap data = new JobDataMap();
         data.put("store", store);
         data.put("parse", parse);
@@ -35,13 +31,34 @@ public class Grabber implements Grab {
                 .usingJobData(data)
                 .build();
         SimpleScheduleBuilder times = simpleSchedule()
-                .withIntervalInSeconds(time)
+                .withIntervalInSeconds(Integer.parseInt(cfg.getProperty("time")))
                 .repeatForever();
         Trigger trigger = newTrigger()
                 .startNow()
                 .withSchedule(times)
                 .build();
         scheduler.scheduleJob(job, trigger);
+    }
+
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes());
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
     public static class GrabJob implements Job {
@@ -63,17 +80,29 @@ public class Grabber implements Grab {
 
 
         public static void main(String[] args) throws Exception {
-            var config = new Properties();
-            try (InputStream input = Grabber.class.getClassLoader()
-                    .getResourceAsStream("app.properties")) {
-                config.load(input);
-            }
-            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-            scheduler.start();
-            var parse = new HabrCareerParse(new HabrCareerDateTimeParser());
-            var store = new PsqlStore(config);
-            var time = Integer.parseInt(config.getProperty("time"));
-            new Grabber(parse, store, scheduler, time).init();
+            Grabber grab = new Grabber();
+            grab.cfg();
+            Scheduler scheduler = grab.scheduler();
+            Store store = grab.store();
+            grab.init(new HabrCareerParse(new HabrCareerDateTimeParser()), store, scheduler);
+            grab.web(store);
+        }
+    }
+
+    private Store store() throws Exception {
+        return new PsqlStore(cfg);
+    }
+
+    private Scheduler scheduler() throws SchedulerException {
+        return StdSchedulerFactory.getDefaultScheduler();
+    }
+
+    private void cfg() {
+        try (InputStream input = Grabber.class.getClassLoader().getResourceAsStream("app.properties")) {
+            cfg = new Properties();
+            cfg.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
